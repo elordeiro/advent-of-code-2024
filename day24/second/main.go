d68b3721b33fae9f8c5a63b057c5bdfc7d2ffbff
elordeiro <estevao.lordeiro@hotmail.com>
2024-12-27 15:45:17 -0600

    Update solution for day 24 part 2

--- day24/second/main.go
+++ day24/second/main.go
@@ -3,25 +3,30 @@ package main
 import (
 	"adventofcode/utils"
 	"fmt"
+	"maps"
 	"slices"
 	"strings"
 )
 
 const (
-	AND = iota
+	Nil = iota
+	AND
 	OR
 	XOR
 )
 
 type Gate struct {
-	in1, in2, out string
-	typ           int
+	in1, in2 string
+	typ      int
 }
 
-func parseInput(fileName string) []Gate {
-	input := utils.ReadStringSlice(fileName)
+type Adder struct {
+	circuit map[string]Gate
+}
 
-	gates := []Gate{}
+func parseInput(fileName string) Adder {
+	input := utils.ReadStringSlice(fileName)
+	circuit := map[string]Gate{}
 
 	var idx int
 	for _, line := range input {
@@ -37,70 +42,120 @@ func parseInput(fileName string) []Gate {
 		var typ int
 		switch parts[1] {
 		case "AND":
-			typ = 0
+			typ = AND
 		case "OR":
-			typ = 1
+			typ = OR
 		case "XOR":
-			typ = 2
+			typ = XOR
 		}
 
 		in1, in2, out := parts[0], parts[2], parts[4]
-		if in2 > in1 {
+		if in1 > in2 {
 			in1, in2 = in2, in1
 		}
-
-		gates = append(gates, Gate{in1, in2, out, typ})
+		circuit[out] = Gate{in1, in2, typ}
 	}
 
-	return gates
+	return Adder{circuit}
+}
+
+func (a Adder) prevGate(x string) int {
+	return a.circuit[x].typ
+}
+
+func (a Adder) prevX(x string) string {
+	return a.circuit[x].in1
+}
+
+// Safe to assume that rule1 is only called for z's
+func (a Adder) rule1(out string, gate Gate) bool {
+	// All z's come from XOR gates, except z45
+	if out == "z45" {
+		if gate.typ != OR {
+			return false
+		}
+	} else if gate.typ != XOR {
+		return false
+	}
+
+	// z00 comes from XOR gate with x00 and y00
+	if out == "z00" {
+		if gate.typ != XOR || (gate.in1 != "x00" || gate.in2 != "y00") {
+			return false
+		}
+		return true
+	}
+
+	// No other z's come 'directly' after x's and y's
+	return !strings.HasPrefix(gate.in1, "x") && !strings.HasPrefix(gate.in2, "y")
+}
+
+func (a Adder) rule2(out string, gate Gate) string {
+	switch gate.typ {
+	case OR:
+		// OR gates's inputs are always AND gates outputs
+		if a.prevGate(gate.in1) != AND {
+			return gate.in1
+		}
+		if a.prevGate(gate.in2) != AND {
+			return gate.in2
+		}
+
+	case AND:
+		// Except for x00, there's never 2 AND gates in a row
+		if a.prevGate(gate.in1) == AND && a.prevX(gate.in1) != "x00" {
+			return gate.in1
+		}
+		if a.prevGate(gate.in2) == AND && a.prevX(gate.in2) != "x00" {
+			return gate.in2
+		}
+
+	case XOR:
+		// XOR gates that come from OR and XOR gates always output to z's
+		prev1 := a.prevGate(gate.in1)
+		prev2 := a.prevGate(gate.in2)
+		if prev1 == XOR && prev2 == OR || prev2 == XOR && prev1 == OR {
+			if !strings.HasPrefix(out, "z") {
+				return out
+			}
+		}
+	}
+
+	return ""
 }
 
 func solve(fileName string) string {
-	gates := parseInput(fileName)
+	res := map[string]struct{}{}
 
-	var edges []string
-	var andID, orID, xorID int
-	for _, gate := range gates {
-		var gateName string
-		var gateID int
-
-		switch gate.typ {
-		case AND:
-			gateID = andID
-			gateName = "AND"
-			andID++
-		case OR:
-			gateID = orID
-			orID++
-			gateName = "OR"
-		case XOR:
-			gateID = xorID
-			xorID++
-			gateName = "XOR"
-		}
-
-		edges = append(edges, fmt.Sprintf("%s --> %s%d(\"%s\") --> %s", gate.in1, gateName, gateID, gateName, gate.out))
-		edges = append(edges, fmt.Sprintf("%s --> %s%d(\"%s\")", gate.in2, gateName, gateID, gateName))
+	adder := parseInput(fileName)
+	var q []string
+	for i := range 46 {
+		q = append(q, fmt.Sprintf("z%02d", i))
 	}
 
-	slices.SortFunc(edges, func(a, b string) int {
-		if a > b {
-			return -1
+	onZs := true
+	for len(q) > 0 {
+		n := len(q)
+		for range n {
+			out := q[0]
+			q = q[1:]
+			if gate, ok := adder.circuit[out]; ok {
+				if onZs && !adder.rule1(out, gate) {
+					res[out] = struct{}{}
+				} else {
+					if r := adder.rule2(out, gate); len(r) > 0 {
+						res[r] = struct{}{}
+					}
+				}
+				q = append(q, gate.in1, gate.in2)
+			}
 		}
-		return 1
-	})
-	for _, e := range edges {
-		fmt.Println(e)
+		onZs = false
 	}
 
-	// Copy output and paste in the live editor for mermaid
-	// https://mermaid.live/edit
-	// Visualy inspect diagram in order to find swapped wires
-	// Swap wires to see if its makes the diagram more 'correct'
-
-	res := []string{"jst", "z05", "gdf", "mcm", "dnt", "z15", "gwc", "z30"}
-	slices.Sort(res)
-	return strings.Join(res, ",")
+	r := slices.Collect(maps.Keys(res))
+	slices.Sort(r)
+	return strings.Join(r, ",")
 }
 
 func main() {
@@ -114,7 +169,7 @@ func main() {
 	for _, test := range tests {
 		got := solve(test.fileName)
 		if got != test.want {
-			fmt.Printf("Failed Test %s\n\tGot %s, Want %s\n", test.fileName, got, test.want)
+			fmt.Printf("Failed Test %s\n\tGot %s\n\tWant %s\n", test.fileName, got, test.want)
 			continue
 		}
 		fmt.Printf("%s: %s\n", test.fileName, got)
